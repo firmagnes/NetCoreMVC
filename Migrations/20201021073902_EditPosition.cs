50f1ba2c97c1cc103174d6d0d096b4f65ed61dfd
firmagnes <firmaagnesramadhan@gmail.com>
2020-10-26 16:49:29 +0700

    Add project files.

---
+++ Migrations/20201021073902_EditPosition.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NetCoreMVC.Migrations
+{
+    public partial class EditPosition : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Position",
+                table: "Employees",
+                type: "varchar(100)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(010)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Position",
+                table: "Employees",
+                type: "varchar(010)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(100)",
+                oldNullable: true);
+        }
+    }
+}
---
+++ Models/EmployeeContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreMVC.Models
+{
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext(DbContextOptions<EmployeeContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set;  }
+    }
+}
